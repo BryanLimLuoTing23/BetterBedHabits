5a7dbacf2c1c76e20c5a4e9a0d2f925394a8b252
Bryan Lim <112593926+BryanLimLuoTing23@users.noreply.github.com>
2023-07-30 17:25:47 +0800

    Update HabitListView.swift

--- HabitListView.swift
+++ HabitListView.swift
@@ -5,17 +5,7 @@ public struct HabitListView: View {
     @State private var isShowingEditView = false
     @State private var editedHabitNames: [String] = []
     
-    init(userHabitData: UserHabitData) {
-        self.userHabitData = userHabitData
-        for index in userHabitData.HabitData.indices{
-            self._editedHabitNames.wrappedValue.append(userHabitData.HabitData[index].habitName)
-        }
-        
-    }
-    //Initially you need a copy of the current data.
-    
     public var body: some View {
-        
         VStack {
             List {
                 ForEach(editedHabitNames.indices, id: \.self) { index in
@@ -40,24 +30,31 @@ public struct HabitListView: View {
         .sheet(isPresented: $isShowingEditView) {
             HabitEditView(editedHabitNames: $editedHabitNames, isShowingEditView: $isShowingEditView)
         }
+        .onAppear {
+            if editedHabitNames.isEmpty {
+                for index in userHabitData.HabitData.indices {
+                    editedHabitNames.append(userHabitData.HabitData[index].habitName)
+                }
+            }
+        }
     }
     
     private func saveAndDismiss() {
         // Update the original userHabitData with the edited habits
-        for index in editedHabitNames.indices{
-            UserHabitData.HabitData[index].habitName = editedHabitNames[index]
+        for index in editedHabitNames.indices {
+            userHabitData.HabitData[index].habitName = editedHabitNames[index]
         }
         
         isShowingEditView = false
     }
 }
 
-struct HabitListItemView: View {
+struct HabitListItemView: View{
     var habitName: String
     
     var body: some View {
         Text(habitName)
             .font(.headline)
     }
+    
 }
-

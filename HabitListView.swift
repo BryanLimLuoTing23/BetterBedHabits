074afb7b803e302f7b0e3460bdddba9c56b6840b
Bryan Lim <112593926+BryanLimLuoTing23@users.noreply.github.com>
2023-07-31 12:05:29 +0800

    Update HabitListView.swift
    Simplified version, with simplified HabitStructType class

--- HabitListView.swift
+++ HabitListView.swift
@@ -1,53 +1,82 @@
+//
+//  HabitListView.swift
+//  Bedtime Bliss
+//
+//  Created by Bryan Lim Luo Ting on 30/07/2023.
+//
+
+import Foundation
 import SwiftUI
 
 public struct HabitListView: View {
     @ObservedObject var userHabitData: UserHabitData
     @State private var isShowingEditView = false
     @State private var editedHabitNames: [String] = []
-    
+
+    func move(from source: IndexSet, to destination: Int) {
+        editedHabitNames.move(fromOffsets: source, toOffset: destination)
+    }
+
+    func deleteItem(at offsets: IndexSet) {
+        editedHabitNames.remove(atOffsets: offsets)
+    }
+
     public var body: some View {
         VStack {
             List {
                 ForEach(editedHabitNames.indices, id: \.self) { index in
-                    HabitListItemView(habitName: editedHabitNames[index])
+                    if isShowingEditView {
+                        TextField("Enter new habit", text: $editedHabitNames[index])
+                    } else {
+                        Text(editedHabitNames[index])
+                    }
                 }
+                .onMove(perform: move)
+                .onDelete(perform: deleteItem)
             }
-            .listStyle(InsetGroupedListStyle())
+            .listStyle(SidebarListStyle())
+
             Spacer()
-            Button(action: {
-                isShowingEditView = true
-            }) {
-                Text("Edit")
-            }
-            Button(action: {
-                saveAndDismiss()
-            }) {
-                Text("Confirm")
+
+            if !isShowingEditView {
+                Button("Edit") {
+                    isShowingEditView = true
+                }
+            } else {
+                Button("Done") {
+                    isShowingEditView = false
+                    updateHabitData()
+                }
             }
         }
         .padding()
-        .navigationBarTitle("Habit List")
-        .sheet(isPresented: $isShowingEditView) {
-            HabitEditView(editedHabitNames: $editedHabitNames, isShowingEditView: $isShowingEditView)
-        }
         .onAppear {
-            if editedHabitNames.isEmpty {
-                for index in userHabitData.HabitData.indices {
-                    editedHabitNames.append(userHabitData.HabitData[index].habitName)
-                }
-            }
-        }
+            editedHabitNames = userHabitData.HabitData.map { $0.habitName }
+        } //Retrieve current habit names into the edit buffer arrap
     }
-    
-    private func saveAndDismiss() {
+
+    private func updateHabitData() {
+        var newHabitData: [HabitModel] = []
         // Update the original userHabitData with the edited habits
         for index in editedHabitNames.indices {
-            userHabitData.HabitData[index].habitName = editedHabitNames[index]
+            newHabitData.append( HabitModel(habitName: editedHabitNames[index],
+                                            
+                duration: 10,
+                
+                                            optionsList: [HabitOption(id: Int( String("\(index+1)0")   )!, option: "Mark as finished"),
+                                                          HabitOption(id: Int(String("\(index+1)1"))!, option: "Unable to complete")
+                                                                                                           ])
+            )
+            
+            userHabitData.HabitData = newHabitData
+            
+         
         }
-        
+
         isShowingEditView = false
     }
 }
+
 
 struct HabitListItemView: View{
     var habitName: String
@@ -58,3 +87,4 @@ struct HabitListItemView: View{
     }
     
 }
+

f13206286b9f14c87a9d304e3687593dde719895
Bryan Lim <112593926+BryanLimLuoTing23@users.noreply.github.com>
2023-07-31 13:45:37 +0800

    Update RoutineCompletedView.swift

--- RoutineCompletedView.swift
+++ RoutineCompletedView.swift
@@ -1,9 +1,26 @@
+//
+//  RoutineCompletedView.swift
+//  Bedtime Bliss
+//
+//  Created by Bryan Lim Luo Ting on 30/07/2023.
+//
+
+import Foundation
 import SwiftUI
 
+
 struct RoutineCompletedView: View {
+    @ObservedObject var userHabitData : UserHabitData
     
     var body: some View {
         Text("Routine finished")
+        
+        Spacer()
+        
+        Button("Restart") {
+            userHabitData.restart()
+        }
+            
     }
     
 }
